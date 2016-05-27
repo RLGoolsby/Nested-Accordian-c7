7ec09e8279559dcf45330eddff5bd2d9baf12cad
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-05-27 17:21:01 -0400

    Add more edits to styles, js, and html

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,7 +1,22 @@
 var tada = document.getElementsByClassName("Food");
+
 for(var i = 0; i < tada.length; i++){
    tada[i].addEventListener("click", function(){
     this.nextElementSibling.classList.toggle("visible");
-
+    this.firstElementChild.classList.toggle("hide-right-arrow");
+    this.lastElementChild.classList.toggle("show-down-arrow");
   });
 }
+
+/******           IDEAS        ***/
+
+// $('div[id^="module-tab-"]').click(function(){
+//          $(this).next('.hi').toggleClass("left-image right-image");
+//     });
+
+// $('#open-close-toggle').on("click", function(){
+//     var $arrows = $(this).find("img");
+//     $('#toggle-section').slideToggle(function(){
+//         $arrows.toggle();
+//     });
+// });
