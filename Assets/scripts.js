8d37f218e90fcafaf81033e98b692a9ce9babbd3
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-05-27 11:51:08 -0400

    Add another html structure

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,3 +1,6 @@
-body{
-
-}
+var taDa = document.getElementById("#closed");
+for(var i = 0; i < taDa.length; i++){
+  taDa[i].addEventListener(.click, function){
+    taDa.addclassName("#open");
+  }
+});
