baf3d6f51dd64ad430db75b3874040f694b08302
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-06-01 17:16:31 -0400

    Add comments

--- Assets/js/jquery.js
+++ Assets/js/jquery.js
@@ -1,33 +1,41 @@
-// $(document).ready(function(){
-//   var tada=$(".Old-school");
-//
-//     $.each(tada, function(i, ".Old-school"){
-//       console.log("Yes " + tada[i]);
-//       var active = ()
-//     });
-//
-// });
-//
 
-/****  save for later ***/
-// $("#card").flip('toggle');
 /*****************************/
+/*** THIS IS THE CODE THAT WILL OPEN AND CLOSE ALL PANELS AT ONCE ***/
+
 // $(".Old-school").click(function(){
 //   $("#1").toggle();
-//   $("#2").toggle();// $("#TV-guide-2").toggle();
 //   $("#3").toggle();
 //   $("#4").toggle();
 //   $("#5").toggle();
 // });
+/*****************************/
 
+/***   THIS IS THE CODE THAT WILL OPEN AND CLOSE THE PANELS INDIVIDUALLY  ***/
 
 $(".Old-school").click(function(){
     $(this).next().toggle();
-    $("i", this).toggleClass("hide-right-arrow");// $(this).toggle(".hide-right-arrow");
+    $("i", this).toggleClass("hide-right-arrow");
     $("i", this).last().toggleClass("show-down-arrow");
   });
+/*****************************/
 
 
+
+/****** CODE ROADS THAT DID NOT CONNECT ***/
+
+  // $(document).ready(function(){
+  //   var tada=$(".Old-school");
+  //
+  //     $.each(tada, function(i, ".Old-school"){
+  //       console.log("Yes " + tada[i]);
+  //       var active = ()
+  //     });
+  //
+  // });
+  //
+
+/****  save for later ideas ***/
+// $("#card").flip('toggle');
 
 
 /***  Second Attempt  ****/
