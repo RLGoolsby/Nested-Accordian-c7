85b5dc0451dfbbfac15ef8fc9f3a2ed6475787d9
Rhonda Goolsby <rlgoolsby@gmail.com>
2016-06-01 14:47:13 -0400

    Add jquery and js script

---
+++ Assets/js/jquery.js
@@ -0,0 +1,44 @@
+// $(document).ready(function(){
+//   var tada=$(".Old-school");
+//
+//     $.each(tada, function(i, ".Old-school"){
+//       console.log("Yes " + tada[i]);
+//       var active = ()
+//     });
+//
+// });
+//
+
+/****  save for later ***/
+// $("#card").flip('toggle');
+/*****************************/
+$(".Old-school").click(function(){
+  $("#1").toggle(".visible");
+  $("#2").toggle(".visible");
+  $("#3").toggle(".visible");
+  $("#4").toggle(".visible");
+  $("#5").toggle(".visible");
+});
+
+  $("#1").click(function(){
+    $("i",this).toggleClass("show-down-arrow");
+  });
+
+
+
+
+/***  Second Attempt  ****/
+
+// $(document).ready(function(){
+//
+//   $( ".Old-school" ).accordion({
+//     event: "click"
+//
+//
+//     var event =$(".Old-school").accordion(".visible", "event");
+//     $(".Old-school").accordion(".visible","active");
+//
+//       var collapsible =$(".Old-school").accordion("option", "collapsible");
+//     $(".Old-school").accordion(".visible","active", true);
+// });
+// });
